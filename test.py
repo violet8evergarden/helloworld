eb88db75329f9324da11fe7312c51ea6d0cba427
violet.evergarden <violet.evergarden@sjtu.edu.cn>
2025-07-24 13:21:01 +0800

    123

--- test.py
+++ test.py
@@ -1,57 +1,52 @@
 import RPi.GPIO as GPIO
 import time
 
-# 设置引脚映射（BCM 模式）
-MotorA = {"IN1": 17, "IN2": 27, "PWM": 22}
-MotorB = {"IN1": 6,  "IN2": 13, "PWM": 19}
-STBY = 5
+# 电机引脚定义
+MOTOR_A = {'IN1': 17, 'IN2': 27, 'ENA': 22}
+MOTOR_B = {'IN1': 6,  'IN2': 13, 'ENA': 19}
 
-def setup():
-    GPIO.setmode(GPIO.BCM)
-    pins = list(MotorA.values()) + list(MotorB.values()) + [STBY]
-    for pin in pins:
-        GPIO.setup(pin, GPIO.OUT)
+# PWM 频率
+PWM_FREQ = 1000
 
-    # 启动 PWM
-    global pwmA, pwmB
-    pwmA = GPIO.PWM(MotorA["PWM"], 1000)  # 1kHz
-    pwmB = GPIO.PWM(MotorB["PWM"], 1000)
-    pwmA.start(0)
-    pwmB.start(0)
+def setup_motor_pins(motor):
+    GPIO.setup(motor['IN1'], GPIO.OUT)
+    GPIO.setup(motor['IN2'], GPIO.OUT)
+    GPIO.setup(motor['ENA'], GPIO.OUT)
+    pwm = GPIO.PWM(motor['ENA'], PWM_FREQ)
+    pwm.start(0)
+    return pwm
 
-def run_motor(motor, speed=100):
-    GPIO.output(motor["IN1"], GPIO.HIGH)
-    GPIO.output(motor["IN2"], GPIO.LOW)
-    if motor == MotorA:
-        pwmA.ChangeDutyCycle(speed)
-    else:
-        pwmB.ChangeDutyCycle(speed)
+def run_motor_forward(motor, pwm, speed=100):
+    GPIO.output(motor['IN1'], GPIO.HIGH)
+    GPIO.output(motor['IN2'], GPIO.LOW)
+    pwm.ChangeDutyCycle(speed)
 
-def stop_motor(motor):
-    GPIO.output(motor["IN1"], GPIO.LOW)
-    GPIO.output(motor["IN2"], GPIO.LOW)
-    if motor == MotorA:
-        pwmA.ChangeDutyCycle(0)
-    else:
-        pwmB.ChangeDutyCycle(0)
+def stop_motor(motor, pwm):
+    GPIO.output(motor['IN1'], GPIO.LOW)
+    GPIO.output(motor['IN2'], GPIO.LOW)
+    pwm.ChangeDutyCycle(0)
 
 def main():
-    setup()
-    GPIO.output(STBY, GPIO.HIGH)  # 使能 TB6612
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
 
-    print("Motor A and B running...")
-    run_motor(MotorA, speed=80)
-    run_motor(MotorB, speed=80)
+    # 设置电机引脚
+    pwm_a = setup_motor_pins(MOTOR_A)
+    pwm_b = setup_motor_pins(MOTOR_B)
+
+    print("Running motors forward for 5 seconds...")
+    run_motor_forward(MOTOR_A, pwm_a)
+    run_motor_forward(MOTOR_B, pwm_b)
 
     time.sleep(5)
 
-    print("Stopping motors...")
-    stop_motor(MotorA)
-    stop_motor(MotorB)
+    print("Stopping motors.")
+    stop_motor(MOTOR_A, pwm_a)
+    stop_motor(MOTOR_B, pwm_b)
 
-    pwmA.stop()
-    pwmB.stop()
+    pwm_a.stop()
+    pwm_b.stop()
     GPIO.cleanup()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
